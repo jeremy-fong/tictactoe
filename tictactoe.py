5d452559735d530596183eb8e89c3d69cae38534
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-28 18:00:54 -0700

    center game window and setup game board format

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,13 @@
 import tkinter
 
+def set_tile(row, column):
+    pass
+
+def new_game():
+    pass
+
 #game setup
-playerx = "x"
+playerx = "X"
 playerO = "O"
 curr_player = playerx
 board = [[0, 0, 0],
@@ -18,4 +24,30 @@ window = tkinter.Tk() #create the game window
 window.title("Tic Tac Toe")
 window.resizable(False, False)
 
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text=curr_player+"'s turn", font=("Consolas", 20), background=color_gray, foreground="white")
+
+label.grid(row=0, column=0, columnspan=3, sticky="we")
+
+for row in range(3):
+    for column in range(3):
+        board[row][column] = tkinter.Button(frame, text="", font=("Consolas", 50, "bold"), background=color_gray, foreground=color_blue, width=4, height=1, command=lambda row=row, column=column: set_tile(row, column))
+        board[row][column].grid(row=row+1, column=column)
+
+button = tkinter.Button(frame, text="Restart", font=("Consolas", 20), background=color_gray, foreground="white", command=new_game)
+button.grid(row=4, column=0, columnspan=3, sticky="we")
+frame.pack()
+
+#center the window
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 window.mainloop() # keeps window open until user exits
