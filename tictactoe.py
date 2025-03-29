f9645a122b304eab0bd60b6032a4cab226ff5e72
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-28 17:31:41 -0700

    added python file and game setup

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+import tkinter
+
+#game setup
+playerx = "x"
+playerO = "O"
+curr_player = playerx
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+
+color_blue = "#4584b6"
+color_yellow = "#ffde57"
+color_gray = "#343434"
+color_light_gray = "#646464"
