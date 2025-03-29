642edc8369d6d9dc1908557ca603162b2a2f298f
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-28 18:41:44 -0700

    added game logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,80 @@
 import tkinter
 
 def set_tile(row, column):
-    pass
+    global curr_player
+
+    if (game_over):
+        return
+
+    if board[row][column]["text"] != "":
+    #spot already taken
+        return
+
+    board[row][column]["text"] = curr_player #mark the board
+
+    if curr_player == playerO:
+        curr_player = playerx
+    else:
+        curr_player = playerO
+
+    label["text"] = curr_player+"'s turn"
+
+    #check winner
+    check_winner()
+
+def check_winner():
+    global turns, game_over
+    turns += 1 
+
+    #horizontally, check 3 rows
+    for row in range(3):
+        if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"] and board[row][0]["text"] != ""):
+            label.config(text=board[row][0]["text"]+" is the winner!", foreground=color_yellow)
+            for column in range(3):
+                board [row][column].config(foreground=color_yellow, background=color_light_gray)
+            game_over = True
+            return
+        
+    #vertically, check 3 column
+    for column in range(3):
+        if (board[0][column]["text"] == board[1][column]["text"] == board[2][column]["text"] and board[0][column]["text"] != ""):
+            label.config(text=board[0][column]["text"]+" is the winner!", foreground=color_yellow)
+            for row in range(3):
+                board [row][column].config(foreground=color_yellow, background=color_light_gray)
+            game_over = True
+            return
+    #diagonally
+    if (board[0][0]["text"]== board[1][1]["text"] == board[2][2]["text"] and board[0][0]["text"] != ""):
+        label.config(text=board[0][0]["text"]+" is the winner!", foreground=color_yellow)
+        for i in range(3):
+            board[i][i].config(foreground=color_yellow, background=color_light_gray)
+        game_over = True
+        return
+    #opposite diagonal
+    if (board[0][2]["text"]== board[1][1]["text"] == board[2][0]["text"] and board[0][2]["text"] != ""):
+        label.config(text=board[0][2]["text"]+" is the winner!", foreground=color_yellow)
+        board[0][2].config(foreground=color_yellow, background=color_light_gray)
+        board[1][1].config(foreground=color_yellow, background=color_light_gray)
+        board[2][0].config(foreground=color_yellow, background=color_light_gray)
+        game_over = True
+        return
+    
+    #tie
+    if (turns == 9):
+        game_over = True
+        label.config(text="Tie!", foreground=color_yellow)
 
 def new_game():
-    pass
+    global turns, game_over
+
+    turns = 0 
+    game_over = False
+
+    label.config(text = curr_player+"'s turn", foreground="white")
+
+    for row in range(3):
+        for column in range(3):
+            board[row][column].config(text ="", foreground=color_blue, background=color_gray)
 
 #game setup
 playerx = "X"
@@ -18,6 +88,9 @@ color_blue = "#4584b6"
 color_yellow = "#ffde57"
 color_gray = "#343434"
 color_light_gray = "#646464"
+
+turns = 0
+game_over = False
 
 #window setup
 window = tkinter.Tk() #create the game window
