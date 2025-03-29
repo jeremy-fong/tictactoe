9a68999bb38ec9c9c4df1040a73b3b3cdfcacccd
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-28 17:36:04 -0700

    added code for window setup

--- tictactoe.py
+++ tictactoe.py
@@ -12,3 +12,10 @@ color_blue = "#4584b6"
 color_yellow = "#ffde57"
 color_gray = "#343434"
 color_light_gray = "#646464"
+
+#window setup
+window = tkinter.Tk() #create the game window
+window.title("Tic Tac Toe")
+window.resizable(False, False)
+
+window.mainloop() # keeps window open until user exits
